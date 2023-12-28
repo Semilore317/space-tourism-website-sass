da478f9180d51be6e3caaa5a2c5967eacb9a30c2
Semilore317 <semi.banks.317@gmail.com>
2023-12-28 22:02:37 +0100

    backdate-trial-2

--- src/Nav.js
+++ src/Nav.js
@@ -4,7 +4,9 @@ function Nav() {
   return (
     <header>
         <nav className='nav'>
-             <div className="logo"></div>
+             <div className="logo">
+                
+             </div>
         </nav>
     </header>
   )
